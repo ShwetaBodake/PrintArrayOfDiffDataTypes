1fc111ae41f43a8867fde8d798f5074f9e23c8fc
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-29 18:59:28 +0530

    Printing Array in different data types

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+namespace PrintArrayDiffDataTypes
+{
+    public class Program
+    {
+        public static void toPrint(int[]  inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+
+        }
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 45, 67, 44, 77, 88 };
+            double[] doubleArray = { 11.22, 44.55, 44.11, 66.2 };
+            char[] charArray = { 'a', 'd', 'f', 'g' };
+
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+        }
+    }
+}
